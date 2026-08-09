ce1fedc573b5b18c99b5199e822b531c60f7f657
afirmae <nilashis37@gmail.com>
2026-08-09 15:44:51 +0530

    feat: add unique slugs to fileMap and include them in document processing

--- app.py
+++ app.py
@@ -30,9 +30,57 @@ def home():
 def custom_static(filename):
     return send_from_directory(os.path.join(app.root_path, 'public', 'assets'), filename)
 
-@app.route('/documents/<path:filename>')
+DOCS_SLUG_MAP = {
+    "aadhar": "Aadhar.pdf",
+    "aadhar-masked": "Aadhar Card - Number Hidden.pdf",
+    "pan": "PAN.pdf",
+    "pan-digilocker": "PAN - Digilocker.pdf",
+    "e-pan": "e-PAN.pdf",
+    "driving-license": "Driving License.pdf",
+    "driving-license-digilocker": "Driving Licence - Digilocker.pdf",
+    "voter-id": "VoterID.pdf",
+    "passport": "Passport.pdf",
+    "10th-marksheet": "Marksheet X.pdf",
+    "10th-admit-card": "Admit Card X.pdf",
+    "10th-marksheet-digilocker": "X Marksheet - Digilocker.pdf",
+    "10th-passing-certificate-digilocker": "X Passing Cerificate - Digilocker.pdf",
+    "12th-marksheet": "Marksheet XII.pdf",
+    "12th-reg-card": "Reg Card XII.pdf",
+    "12th-marksheet-certificate-digilocker": "XII Marksheet and Certificate.pdf",
+    "sem1": "Result Sem 1.pdf",
+    "sem1-digilocker": "SEM 1 - Digilocker.pdf",
+    "sem2": "Result Sem 2.pdf",
+    "sem2-digilocker": "SEM 2 - Digilocker.pdf",
+    "sem3": "Result Sem 3.pdf",
+    "sem3-digilocker": "SEM 3 - Digilocker.pdf",
+    "sem4": "Result Sem 4.PDF",
+    "sem4-digilocker": "SEM 4 - Digilocker.pdf",
+    "sem5": "Result Sem 5.pdf",
+    "sem5-digilocker": "SEM 5 - Digilocker.pdf",
+    "sem6": "Result Sem 6.pdf",
+    "sem6-digilocker": "SEM 6 - Digilocker.pdf",
+    "sem7": "Result Sem 7.pdf",
+    "sem7-digilocker": "SEM 7 - Digilocker.pdf",
+    "sem8": "Result Sem 8.pdf",
+    "all-results": "All Results.pdf",
+    "abcid-digilocker": "ABCID - Digilocker.pdf",
+    "birth-certificate": "Birth Certificate.pdf",
+    "domicile": "Domicile.pdf",
+    "passbook": "Pass Book Front Page.pdf",
+    "ration-card-digilocker": "Ration Card - Digilocker.pdf",
+    "sc-certificate": "SC Certificate.pdf"
+}
+
+@app.route('/docs/<slug>')
 @limiter.exempt
-def serve_documents(filename):
+def serve_secure_document(slug):
+    if request.cookies.get('docs_access') != 'true':
+        return redirect('/docs')
+        
+    filename = DOCS_SLUG_MAP.get(slug)
+    if not filename:
+        return render_template('404.html', version=VERSION), 404
+        
     return send_from_directory(os.path.join(app.root_path, 'public', 'documents'), filename)
 
 @app.route('/docs')
